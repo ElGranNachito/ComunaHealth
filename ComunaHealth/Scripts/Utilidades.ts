dc17e42c3f7380d23750c069fba3335f5bc676ca
Crazy474747 <58375872+Crazy474747@users.noreply.github.com>
2021-09-24 19:59:54 -0300

    Ver descripcion
    -Añadido typescript
    -Boton registrarse ahora lleva a la nueva pagina de registro

---
+++ ComunaHealth/Scripts/Utilidades.ts
@@ -0,0 +1,24 @@
+﻿
+function SeleccionarItemDropdown(indice: number, idBoton: string, idInput: string) {
+
+    switch (indice)
+    {
+
+        case 0:
+            CambiarValorSeleccionadoDropdown("Paciente", idBoton, idInput);
+            break;
+
+        case 1:
+            CambiarValorSeleccionadoDropdown("Doctor", idBoton, idInput);
+            break;
+
+        default:
+            console.log("Tipo de cuenta no reconocido");
+    }
+}
+
+function CambiarValorSeleccionadoDropdown(nuevoValor: string, idBoton: string, idInput: string)
+{
+    document.getElementById(idBoton).innerText = nuevoValor;
+    (<HTMLInputElement>document.getElementById(idInput)).value = nuevoValor;
+}
