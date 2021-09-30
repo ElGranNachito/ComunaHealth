f7ee70e31d8715e77bb4eed506e951057b9313cf
Contykpo <samu.1furlanich@gmail.com>
2021-09-30 18:16:32 -0300

    Descripcion
    - Tablas intermedias entre modelos y relaciones en db context.

--- ComunaHealth/Modelos/Entradas/ModeloContenedorDeEntradas.cs
+++ ComunaHealth/Modelos/Entradas/ModeloContenedorDeEntradas.cs
@@ -17,11 +17,17 @@ namespace ComunaHealth.Modelos
         /// Clave para desencriptar el contenedor con sus entradas.
         /// </summary>
         public string ClaveEncriptado { get; set; }
-        
+    }
+
+    /// <summary>
+    /// Modelo de datos que representa a un contenedor de entradas.
+    /// </summary>
+    public class ModeloContenedorDeEntradas<TIEntrada> : ModeloBase
+    {
         /// <summary>
         /// Entradas contenidas.
         /// </summary>
-        public List<TIContenedorDeEntradasEntrada> Entradas { get; set; }
+        public List<TIContenedorDeEntradasEntrada<TIEntrada>> Entradas { get; set; } = new List<TIContenedorDeEntradasEntrada<TIEntrada>>();
     }
 
     /// <summary>
@@ -32,6 +38,6 @@ namespace ComunaHealth.Modelos
         /// <summary>
         /// Entradas contenidas.
         /// </summary>
-        public List<TIChatUsuario> Participantes { get; set; }
+        public List<TIChatUsuario> Participantes { get; set; } = new List<TIChatUsuario>();
     }
 }
